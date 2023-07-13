dba521c0ca7623c08886ef7be7619e53b2812a0a
Rahma <s3901990@student.rmit.edu.au>
2023-07-13 19:15:07 +1000

    lesson card updated to change book icon on hover

--- frontend/src/components/LessonCard.js
+++ frontend/src/components/LessonCard.js
@@ -1,17 +1,36 @@
 import React, { useState } from "react";
 import "./LessonCard.css";
+import { Link } from "react-router-dom";
 
 export default function LessonCard(props){
     // lesson for now just contains lesson tile (lessons = array of lesson titles)
     const { lesson } = props;
 
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    }
+
     return(
         <>
-        <div className="lesson-card">
-        <img className="book"></img>
-        {lesson}
-        
-        </div>
+        <Link className="lesson-card" 
+            to="/"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            {isHovered ? 
+            (<img className="book-icon" src={process.env.PUBLIC_URL+"/fi-sr-book-alt.svg"}></img>)
+            : 
+            (<img className="book-icon" src={process.env.PUBLIC_URL+"/fi-rr-book-alt.svg"}></img>)
+            }
+
+            <div className="week-text">{lesson}</div>
+        </Link>  
         </>
     )
 }
