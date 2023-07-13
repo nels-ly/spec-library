5e8c1e2633f2bd835c0f79770166d9db37ede5d6
Rahma <s3901990@student.rmit.edu.au>
2023-07-13 20:40:38 +1000

    lesson pagge navbar

---
+++ frontend/src/pages/LessonPage.js
@@ -0,0 +1,12 @@
+import {React} from "react";
+import "./LessonPage.css"
+import NavBar from "../components/NavBar";
+
+export default function CoursePage() {
+
+    return(
+        <>
+            <NavBar user_role="student"></NavBar>
+        </>
+    )
+}
