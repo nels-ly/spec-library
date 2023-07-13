43beed80a2637a673b6d74d5bbb208784887b7b1
Rahma <s3901990@student.rmit.edu.au>
2023-07-13 19:32:58 +1000

    Lesson Page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,14 @@ import LoginPage from "./pages/LoginPage";
 import DashBoard from "./pages/DashBoard";
 import SignUpPage from "./pages/SignUpPage";
 import CoursePage from "./pages/CoursePage";
+import LessonPage from "./pages/LessonPage";
 
 // define paths 
 const ROOT = "/";  // Login page
 const DASHBOARD  = "/dashboard";
 const SIGNUP = "/signup";
 const COURSE_PAGE = "/course";
+const LESSON_PAGE = "/lesson"
 
 export let LoggedIn = true;
 
@@ -25,6 +27,7 @@ export default function App() {
                 <Route path={DASHBOARD} element={<DashBoard />} />
                 <Route path={SIGNUP} element={<SignUpPage />} />
                 <Route path={COURSE_PAGE} element={<CoursePage/>}/>
+                <Route path={LESSON_PAGE} element={<LessonPage/>}/>
             </Routes>
         </Router>
         </>
