227206e744c04a8a001fd501131263ded67f66a3
nely1 <nely@student.unimelb.edu.au>
2023-07-20 20:10:22 +1000

    changed some models and views

--- backend/user_app/urls.py
+++ backend/user_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.login, name="login"),
+    path('login/', views.login, name="login"),
     path('sign_up/', views.sign_up, name="sign_up")
 ]
--- backend/user_app/views.py
+++ backend/user_app/views.py
@@ -1,58 +1,63 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.renderers import JSONRenderer 
-from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from django.contrib.auth.hashers import make_password, check_password
+from rest_framework.status import HTTP_404_NOT_FOUND,HTTP_401_UNAUTHORIZED, HTTP_200_OK, HTTP_409_CONFLICT
+from rest_framework.authentication import SessionAuthentication
+from rest_framework import permissions
+
+
 
 from .serializers import *
 
 # Create your views here.
 
-@api_view(['POST', 'GET'])
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([permissions.AllowAny])
 def login(request):
     
     try:
+        data = request.data
+
+        user = Spectrum_user.objects.get(email=data['email'])
         
-        email = 'ly@gmail.com'
-        password = '1234'
-       
-        user = Spectrum_user.objects.get(email=email)
+        serializer = Spectrum_profile_serializer(user, many=False)
         
-        serializer = Spectrum_user_serializer(user, many=False)
+        if  not check_password(data['password'], user.password):
+            return Response("Incorrect details",status=HTTP_401_UNAUTHORIZED )
         
-        if  not check_password(password, user.password):
-            return HttpResponse("incorrect details")
         
-        return Response(serializer.data)
+        if 'user' not in request.session:
+            request.session['user'] = serializer.data
         
-    except Exception as e:
-        raise e
+        response_data = serializer.data
+        response = Response(response_data, content_type='application/json', status=HTTP_200_OK)
+        return response
+        
+    except Spectrum_user.DoesNotExist:
+        return Response(data = {'message': "User does not exist"}, status= HTTP_404_NOT_FOUND)
         
 
 
-@api_view(['POST', 'GET'])
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
 def sign_up(request):
     
     try:
-        data = {'first_name': 'nelson', 
-                'last_name': 'ly',
-                'email': 'ly@gmail.com',
-                'password': '1234',
-                'gender':'M',
-                'username': 'chuuniham'
-                }
+        data = request.data
         
         data['password'] = make_password(data['password'])
         
-        if not Spectrum_user.objects.get(email=data['email']):
+        
+        if not Spectrum_user.objects.filter(email=data['email'], username=data['username']).exists():
             serializer = Spectrum_user_serializer(data = data)
             if serializer.is_valid():
                 serializer.save()
         else:
-            return Response('user already exists')
+            return Response('User already exists', status = HTTP_409_CONFLICT)
         
-        return Response("it worked")
+        return Response("Account created")
     
     except Exception as e:
         raise e
