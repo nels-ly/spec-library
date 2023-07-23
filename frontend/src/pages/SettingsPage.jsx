6810964ff86b2dc7a76c268ea7573f016bd936b6
Rahma <s3901990@student.rmit.edu.au>
2023-07-23 23:00:54 +1000

    username

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,17 +1,28 @@
-import {React} from "react";
+import {React, useEffect, useState} from "react";
 import "./DashBoard.css";
 import CoursePage from "./CoursePage";
 import CourseCard from "../components/CourseCard"
 import NavBar from "../components/NavBar";
 import { Link, useLocation} from "react-router-dom";
+import Cookies from "js-cookie";
 
 export default function SettingsPage() {
     const location = useLocation();
-    const { user_role } = location.state || { user_role: "tutor" };
+    const [user, setUser ] = useState(false);
 
-    return (<>
-         <NavBar user_role={user_role}></NavBar>
-    
+    useEffect(()=>{
+        setUser(JSON.parse(Cookies.get("ple")))
+
+    }, []) 
+
+    console.log(user)
+
+    return (
+    <>
+         <NavBar user_role={user.role} username={user.username}></NavBar>
+
+        <h1 className="profile-title">Account Details</h1>
+
     </>)
 
 
